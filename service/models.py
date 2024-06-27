1ca32efb54438cd91141dc953eac1742afc950ae
Mingyi Lim <mingyi850@gmail.com>
2024-06-27 03:45:32 +0000

    Added serde logic

--- service/models.py
+++ service/models.py
@@ -6,6 +6,7 @@ All of the models are stored in this module
 
 import logging
 import enum
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -24,6 +25,18 @@ class PromotionType(enum.Enum):
     PERCENTAGE = 1
     ABSOLUTE = 2
 
+    @classmethod
+    def deserialize(cls, promotion_type_str: str):
+        """ Convert promotion_type_str into a PromotionType or None"""
+        if promotion_type_str is None:
+            return None
+        try:
+            return PromotionType[promotion_type_str]
+        except KeyError as error:
+            raise DataValidationError(
+                f"Error: '{promotion_type_str}' is not a valid PromotionType"
+            ) from error
+
 
 class PromotionScope(enum.Enum):
     """Used to represent the scope a promotion is applicable to"""
@@ -31,6 +44,18 @@ class PromotionScope(enum.Enum):
     PRODUCT_ID = 1
     PRODUCT_CATEGORY = 2
     ENTIRE_STORE = 3
+
+    @classmethod
+    def deserialize(cls, promotion_scope_str: str):
+        """Convert promotion_scope_str into a PromotionType or None"""
+        if promotion_scope_str is None:
+            return None
+        try:
+            return PromotionScope[promotion_scope_str]
+        except KeyError as error:
+            raise DataValidationError(
+                f"Error: '{promotion_scope_str}' is not a valid PromotionType"
+            ) from error
 
 
 class Promotion(db.Model):
@@ -97,17 +122,69 @@ class Promotion(db.Model):
 
     def serialize(self):
         """Serializes a YourResourceModel into a dictionary"""
-        return {"id": self.promotion_id, "name": self.promotion_name}
+        return {
+            "promotion_id": self.promotion_id,
+            "promotion_name": self.promotion_name,
+            "promotion_description": self.promotion_description,
+            "promotion_type": self.promotion_type,
+            "promotion_scope": self.promotion_scope,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "promotion_value": self.promotion_value,
+            "promotion_code": self.promotion_code,
+            "created_by": self.created_by,
+            "modified_by": self.modified_by,
+            "created_when": self.created_when,
+            "modified_when": self.modified_when,
+        }
+
+    def deserialize_datetime(self, datetime_str: str):
+        """
+        Deserialize a datetime from a datetime string
+        Args:
+            datetime_str: A string representing the date or None
+        """
+        if datetime_str is None:
+            return None
+        try:
+            formats = ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d']
+            for fmt in formats:
+                try:
+                    return datetime.strptime(datetime_str, fmt)
+                except ValueError:
+                    continue
+            raise ValueError(
+                f"Time data '{datetime_str}' does not match any of the expected formats."
+            )
+        except ValueError as error:
+            raise DataValidationError(
+                f"Invalid date format: {datetime_str} does not conform to any valid datetime format"
+            ) from error
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
-
+        Deserializes a Promotion from a dictionary
         Args:
-            data (dict): A dictionary containing the resource data
+            data (dict): A dictionary containing the promotion data
         """
         try:
-            self.promotion_name = data["name"]
+            self.promotion_id = data.get("promotion_id", None)
+            self.promotion_name = data.get("promotion_name", None)
+            self.promotion_description = data.get("promotion_description", None)
+            self.promotion_type = PromotionType.deserialize(data.get("promotion_type", None))
+            self.promotion_scope = PromotionScope.deserialize(
+                data.get("promotion_scope", None)
+            )
+            self.start_date = self.deserialize_datetime(data.get("start_date", None))
+            self.end_date = self.deserialize_datetime(data.get("end_date", None))
+            self.promotion_value = data.get("promotion_value", None)
+            self.promotion_code = data.get("promotion_code", None)
+            self.created_by = data.get("created_by", None)
+            self.modified_by = data.get("modified_by", None)
+            self.created_when = self.deserialize_datetime(data.get("created_when", None))
+            self.modified_when = self.deserialize_datetime(data.get(
+                "modified_when", None
+            ))
         except AttributeError as error:
             raise DataValidationError("Invalid attribute: " + error.args[0]) from error
         except KeyError as error:
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,10 +4,11 @@ Test cases for Pet Model
 
 import os
 import logging
+import uuid
 from datetime import datetime
 from unittest import TestCase
 from wsgi import app
-from service.models import Promotion, DataValidationError, db
+from service.models import Promotion, DataValidationError, PromotionScope, PromotionType, db
 from .factories import PromotionFactory
 
 DATABASE_URI = os.getenv(
@@ -50,6 +51,95 @@ class TestPromotions(TestCase):
     ######################################################################
 
     # Todo: Add your test cases here...
+
+    def test_serialize_promotion(self):
+        """ It should deserialize a Promotion into a dict"""
+        test_promotion = PromotionFactory()
+        serialized = test_promotion.serialize()
+        assert serialized["promotion_id"] == test_promotion.promotion_id
+        assert serialized["promotion_name"] == test_promotion.promotion_name
+        assert serialized["promotion_type"] == test_promotion.promotion_type
+        assert serialized["promotion_scope"] == test_promotion.promotion_scope
+        assert serialized["start_date"] == test_promotion.start_date
+        assert serialized["end_date"] == test_promotion.end_date
+        assert serialized["promotion_value"] == test_promotion.promotion_value
+        assert serialized["promotion_code"] == test_promotion.promotion_code
+        assert serialized["created_by"] == test_promotion.created_by
+        assert serialized["modified_by"] == test_promotion.modified_by
+        assert serialized["created_when"] == test_promotion.created_when
+        assert serialized["modified_when"] == test_promotion.modified_when
+
+    def test_serialized_promotion_with_missing_fields(self):
+        """ It should deserialize a Promotion into a dict with some fields as None"""
+        test_promotion = PromotionFactory()
+        test_promotion.promotion_name = None
+        test_promotion.modified_when = None
+        serialized = test_promotion.serialize()
+        assert serialized["promotion_id"] == test_promotion.promotion_id
+        assert serialized["promotion_type"] == test_promotion.promotion_type
+        assert serialized["promotion_name"] is None
+        assert serialized["modified_when"] is None
+
+    def test_deserialize_promotion(self):
+        """It should deserialize a promotion json into a valid Promotion"""
+        default_uuid = uuid.uuid4()
+        promotion_json = {
+            "promotion_id": 123,
+            "promotion_name": "abcPromotion",
+            "promotion_description": "abcPromotionDescription",
+            "promotion_type": "PERCENTAGE",
+            "promotion_scope": "PRODUCT_ID",
+            "start_date": "2021-01-01",
+            "end_date": "2021-12-31",
+            "promotion_value": 10.0,
+            "promotion_code": "abcPromotionCode",
+            "created_by": default_uuid,
+            "modified_by": default_uuid,
+            "created_when": "2021-01-01",
+            "modified_when": "2021-01-01",
+        }
+        promotion = Promotion()
+        promotion.deserialize(promotion_json)
+        assert promotion.promotion_id == 123
+        assert promotion.promotion_name == "abcPromotion"
+        assert promotion.promotion_description == "abcPromotionDescription"
+        assert promotion.promotion_type == PromotionType.PERCENTAGE
+        assert promotion.promotion_scope == PromotionScope.PRODUCT_ID
+        assert promotion.start_date == datetime(2021, 1, 1, 0, 0)
+        assert promotion.end_date == datetime(2021, 12, 31, 0, 0)
+        assert promotion.promotion_value == 10.0
+        assert promotion.promotion_code == "abcPromotionCode"
+        assert promotion.created_by == default_uuid
+        assert promotion.modified_by == default_uuid
+        assert promotion.created_when == datetime(2021, 1, 1, 0, 0)
+        assert promotion.modified_when == datetime(2021, 1, 1, 0, 0)
+
+    def test_deserialize_invalid_promotion(self):
+        """ It should throw an exception for any invalid formats """
+        invalid_json_type = {
+            "promotion_id": 123,
+            "promotion_name": "abcPromotion",
+            "promotion_description": "abcPromotionDescription",
+            "promotion_type": "InvalidFormat",
+        }
+        invalid_json_scope = {
+            "promotion_id": 123,
+            "promotion_name": "abcPromotion",
+            "promotion_description": "abcPromotionDescription",
+            "promotion_scope": "SOME UNKNOWN",
+        }
+        invalid_json_date = {
+            "promotion_id": 123,
+            "promotion_name": "abcPromotion",
+            "promotion_description": "abcPromotionDescription",
+            "promotion_scope": "SOME UNKNOWN",
+            "start_date": "abc123"
+        }
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, invalid_json_type)
+        self.assertRaises(DataValidationError, promotion.deserialize, invalid_json_scope)
+        self.assertRaises(DataValidationError, promotion.deserialize, invalid_json_date)
+
     def test_create_promotion(self):
         """It should create a Promotion"""
         test_promotion = PromotionFactory()
