3f22fe64e401ab5501872396e14f18ef40c4a429
Mingyi Lim <mingyi850@gmail.com>
2024-07-26 04:35:46 +0000

    Fix issues with test

--- features/promotions.feature
+++ features/promotions.feature
@@ -6,10 +6,18 @@ Feature: The promotion service back-end
 Background:
     Given the following promotions
         | ID | Name | Value | Code | Description | Type | Active | Scope | Start Date | End Date | Created By | Modified By | Created When | Modified When |
-        | 1 | Promotion A | 10 | CODE1 | Description 1 | Type 1 | True | Scope 1 | 2024-01-01 | 2024-12-31 | User 1 | User 2 | 2024-01-01 | 2024-01-02 |
-        | 2 | Promotion B | 20 | CODE2 | Description 2 | Type 2 | False | Scope 2 | 2024-02-01 | 2024-11-30 | User 3 | User 4 | 2024-02-01 | 2024-02-02 |
+        | 1 | Promotion A | 10 | CODE1 | Description 1 | ABSOLUTE | True | ENTIRE_STORE | 2024-01-01 | 2024-12-31 | 00000000-0000-0000-0000-000000000000 | 00000000-0000-0000-0000-000000000000 | 2024-01-01 | 2024-01-02 |
+        | 2 | Promotion B | 20 | CODE2 | Description 2 | PERCENTAGE | False | PRODUCT_ID | 2024-02-01 | 2024-11-30 | 00000000-0000-0000-0000-000000000000 | 00000000-0000-0000-0000-000000000000 | 2024-02-01 | 2024-02-02 |
 
 Scenario: The server is running
     When I visit the "Home Page"
     Then I should see "Promotion Demo RESTful Service" in the title
     And I should not see "404 Not Found"
+
+Scenario: Retrieving a promotion
+    When I visit the "Home Page"
+    And I look for test promotion id "1"
+    And I click the "retrieve-btn" button
+    Then I should see the promotion details in the form
+    | ID | Name | Value | Code | Description | Type | Active | Scope | Start Date | End Date | Created By | Modified By | Created When | Modified When |
+    | 1 | Promotion A | 10 | CODE1 | Description 1 | ABSOLUTE | True | ENTIRE_STORE | 2024-01-01 | 2024-12-31 | 00000000-0000-0000-0000-000000000000 | 00000000-0000-0000-0000-000000000000 | 2024-01-01 | 2024-01-02 |
--- features/steps/promotions_steps.py
+++ features/steps/promotions_steps.py
@@ -3,7 +3,41 @@ import os
 import requests
 from behave import given, when, then
 from datetime import datetime
+from compare3 import expect
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_404_NOT_FOUND = 404
+
+WAIT_TIMEOUT = 10
+
+def read_promotion_from_table(row):
+    promotion = {
+        "promotion_id": int(row["ID"]),
+        "promotion_name": row["Name"],
+        "promotion_value": int(row["Value"]),
+        "promotion_code": row["Code"],
+        "promotion_description": row["Description"],
+        "promotion_type": row["Type"],
+        "active": row["Active"].lower() == "true",
+        "promotion_scope": row["Scope"],
+        "start_date": row["Start Date"],
+        "end_date": row["End Date"],
+        "created_by": row["Created By"],
+        "modified_by": row["Modified By"],
+        "created_when": row["Created When"],
+        "modified_when": row["Modified When"]
+    }
+    return promotion
+
+def to_actual_promotion_id(context, test_promotion_id):
+    return str(context.promotions[int(test_promotion_id) - 1])
 
 @given('the server is started')
 def step_impl(context):
@@ -13,25 +47,25 @@ def step_impl(context):
 
 @given('the following promotions')
 def step_impl(context):
+    # Get a list all of the promotions
+    rest_endpoint = f"{context.base_url}/promotions"
+    context.resp = requests.get(rest_endpoint, timeout=10)
+    expect(context.resp.status_code).equal_to(200)
+    # and delete them one by one
+    for promotion in context.resp.json():
+        context.resp = requests.delete(
+            f"{rest_endpoint}/{promotion['promotion_id']}", timeout=WAIT_TIMEOUT
+        )
+        expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
+
     promotions = []
     for row in context.table:
-        promotion = {
-            "id": int(row["ID"]),
-            "name": row["Name"],
-            "value": int(row["Value"]),
-            "code": row["Code"],
-            "description": row["Description"],
-            "type": row["Type"],
-            "active": row["Active"].lower() == "true",
-            "scope": row["Scope"],
-            "start_date": datetime.strptime(row["Start Date"], "%Y-%m-%d"),
-            "end_date": datetime.strptime(row["End Date"], "%Y-%m-%d"),
-            "created_by": row["Created By"],
-            "modified_by": row["Modified By"],
-            "created_when": datetime.strptime(row["Created When"], "%Y-%m-%d"),
-            "modified_when": datetime.strptime(row["Modified When"], "%Y-%m-%d"),
-        }
-        promotions.append(promotion)
+        promotion = read_promotion_from_table(row)
+        context.resp = requests.post(rest_endpoint, json=promotion, timeout=WAIT_TIMEOUT)
+        print(context.resp.text)
+        expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
+        id = context.resp.json()["promotion_id"]
+        promotions.append(id)
     context.promotions = promotions
 
 
@@ -39,7 +73,24 @@ def step_impl(context):
 def step_impl(context):
     context.base_url = os.getenv('BASE_URL', 'http://localhost:8080')
     context.resp = requests.get(context.base_url + '/')
-    assert context.resp.status_code == 200
+    context.driver.get(context.base_url + "/")
+
+
+@when('I look for test promotion id "{test_promotion_id}"')
+def step_impl(context, test_promotion_id):
+    field = context.driver.find_element(By.ID, "promotion_id")
+    actual_promotion_id = to_actual_promotion_id(context, test_promotion_id)
+    field.send_keys(actual_promotion_id)
+
+@when('I enter "{fill}" into the "{element_id}" field')
+def step_impl(context, fill, element_id):
+    field = context.driver.find_element(By.ID, element_id)
+    field.send_keys(fill)
+
+@when('I click the "{element_id}" button')
+def step_impl(context, element_id):
+    button = context.driver.find_element(By.ID, element_id)
+    button.click()
 
 
 @then('I should see "{message}" in the title')
@@ -50,3 +101,21 @@ def step_impl(context, message):
 @then('I should not see "404 Not Found"')
 def step_impl(context):
     assert "404 Not Found" not in str(context.resp.text)
+
+def expect_field_value(context, field_id, expected):
+    element = context.driver.find_element(By.ID, field_id)
+    value = element.get_attribute("value")
+    print("Got", value, "Expected", expected)
+    assert value == expected
+
+@then('I should see the promotion details in the form')
+def step_impl(context):
+    expected_promotion = read_promotion_from_table(context.table[0])
+    expected_promotion_id = to_actual_promotion_id(context, expected_promotion["promotion_id"])
+    WebDriverWait(context.driver, 3).until(
+        expected_conditions.text_to_be_present_in_element_value((By.ID, "promotion_id"), expected_promotion_id)
+    )
+    print(context.driver.page_source)
+    expect_field_value(context, "promotion_id", expected_promotion_id)
+    expect_field_value(context, "promotion_name", str(expected_promotion["promotion_name"]))
+    assert True
