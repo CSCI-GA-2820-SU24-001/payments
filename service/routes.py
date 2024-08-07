816ae4b94573dab29657185ee552b4a1c4ab609d
hrithik2200 <hrithikdhoka@gmail.com>
2024-08-07 13:21:50 +0000

    bug fix for CD deployment

--- service/routes.py
+++ service/routes.py
@@ -43,7 +43,7 @@ def index():
 ######################################################################
 
 
-@api.route("/health")
+@api.route("/api/health")
 class HealthResource(Resource):
     """ Healthcheck endpoints
     GET /health - Returns status of the service
